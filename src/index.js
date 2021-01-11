ac47ac56618bb75f11a0e1f6854c1f1cc76e55c5
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-11 15:19:50 -0500

    updated index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'unfetch/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
+import App from './components/App/App';
+import './setup-icons';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
