3211a83b62cf70dc7cb1858539d0c8f668dea732
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-11 22:22:53 -0500

    created DashCard.css

---
+++ src/components/DashCard/DashCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './DashCard.css';
+
+export default function DashCard(props){
+    return props.words.map(word =>{
+        return (
+            <div className="DashCard" key={word.original}>
+                <h4 className="Word">{word.original}</h4>
+                <span>
+                    correct answer count: {word.correct_count}
+                </span>
+                <br/>
+                <span>
+                    incorrect answer count: {word.incorrect_count}
+                </span>
+            </div>
+        )
+    })
+};
