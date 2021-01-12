d9fa6dee8a032012304751a96cd975b437d7f1d5
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-11 21:28:54 -0500

    created DashboardRoute.js

---
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Dashboard from '../../components/DashBoard/DashBoard';
+
+class DashboardRoute extends Component {
+  render() {
+    return (
+      <section>
+        <Dashboard/>
+      </section>
+    );
+  }
+};
+
+export default DashboardRoute;
