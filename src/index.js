116bee5bfe4dc22537ff401697ecc03f18d8a016
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-12 14:28:26 -0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import 'unfetch/polyfill'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { UserProvider } from './contexts/UserContext'
-import App from './components/App/App'
-import './setup-icons'
-import './index.css'
-import * as serviceWorker from './serviceWorker'
+import 'unfetch/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
+import App from './components/App/App';
+import './setup-icons';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -17,4 +17,4 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
-serviceWorker.unregister()
+serviceWorker.unregister();
