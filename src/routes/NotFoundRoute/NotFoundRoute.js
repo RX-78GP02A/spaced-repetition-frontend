2aef30b381672d3aa9c90f677be60578ffe40ef5
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-11 21:15:58 -0500

    created NotFoundRoute.js

---
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class NotFoundRoute extends Component {
+  render() {
+    return (
+      <section className="not-found-page">
+        <h1>404 - Page not found</h1>
+        <p>Try going back to your previous page.</p>
+      </section>
+    );
+  }
+}
+
+export default NotFoundRoute
