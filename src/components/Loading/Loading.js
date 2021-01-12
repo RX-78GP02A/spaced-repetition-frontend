224378d6adab9dcdd95901d7cb96591ecd612167
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-12 11:24:58 -0500

    created Loading.js

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MagicSpinner } from 'react-spinners-kit';
+
+export default function Loading(props) {
+  return (
+    <div className="loading-container">
+      <MagicSpinner
+          size={50}
+          color="darkslategrey"
+          className="loading"
+          loading={props.loading}
+      />
+    </div>   
+  )
+};
