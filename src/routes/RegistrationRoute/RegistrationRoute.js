9b1f0ae46b2f6e1cbfb3d68a214b85d891cc3caf
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-11 20:46:05 -0500

    created RegistrationRoute

---
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import './RegistrationRoute.css';
+
+class RegistrationRoute extends Component {
+  static defaultProps = {
+    history: {
+      push: () => {},
+    },
+  };
+
+  handleRegistrationSuccess = () => {
+    const { history } = this.props
+    history.push('/login')
+  };
+
+  render() {
+    return (
+      <section className="registration-page">
+        <p>
+          Practice learning a language with the spaced reptition revision technique.
+        </p>
+        <h2 className="sign-up">Sign Up</h2>
+        <RegistrationForm
+          onRegistrationSuccess={this.handleRegistrationSuccess}
+        />
+      </section>
+    );
+  }
+};
+
+export default RegistrationRoute;
