3d40188f06537288e635bf54c3ec6e7ed32f90ef
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-11 20:31:18 -0500

    updated idle-service.js

--- src/services/idle-service.js
+++ src/services/idle-service.js
@@ -1,13 +1,13 @@
-let _timeoutId
-let _idleCallback = null
+let _timeoutId;
+let _idleCallback = null;
 let _notIdleEvents = [
   'mousedown',
   'mousemove',
   'keypress',
   'scroll',
   'touchstart',
-]
-const _FIVE_MINUTES_IN_MS = 5 * 60 * 1000
+];
+const _FIVE_MINUTES_IN_MS = 5 * 60 * 1000;
 
 const IdleService = {
   setIdleCallback(idleCallback) {
@@ -36,6 +36,6 @@ const IdleService = {
       )
     )
   },
-}
+};
 
-export default IdleService
+export default IdleService;
