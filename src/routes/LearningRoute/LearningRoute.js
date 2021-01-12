8a1a351c951577a704abda3677c3a277b63d8a71
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-11 21:27:41 -0500

    created LearningRoute.js

---
+++ src/routes/LearningRoute/LearningRoute.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import LearningPage from '../../components/LearningPage/LearningPage';
+
+class LearningRoute extends Component {
+  render() {
+    return (
+      <section>
+        <LearningPage />
+      </section>
+    );
+  }
+};
+
+export default LearningRoute;
